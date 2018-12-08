81604312dd9726a559145a4f448fa0edfde7cee4
GregorFernbach <gregor.fernbach.2@mailbox.org>
2018-12-08 14:45:33 +0100

    update lab1

--- lab1/src/main/java/at/fh/ima/swengs/moviedb/model/ActorRepository.java
+++
@@ -1,8 +0,0 @@
-package at.fh.ima.swengs.moviedb.model;
- 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
- 
-@Repository()
-public interface ActorRepository extends PagingAndSortingRepository<Actor,Long> {
-}
