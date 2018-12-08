53f311ec254487c56dc346f7657f50765f062297
GregorFernbach <gregor.fernbach.2@mailbox.org>
2018-12-08 14:41:44 +0100

    Adding lab7 integrating bootstrap in Angular and correct REST integration

---
+++ lab7 - Angular_Forms/frontend/src/app/api/actor.ts
@@ -0,0 +1,10 @@
+export interface Actor {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  rating?: number;
+  dayOfBirth?: Date;
+  gender?: string;
+  alive?: string;
+  movies?: Array<any>;
+}
